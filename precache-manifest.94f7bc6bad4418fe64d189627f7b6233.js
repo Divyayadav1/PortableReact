fb2874109130f7e23b755849c53b694a429eb7cc
Divyayadav1 <divyams88@gmail.com>
2020-09-15 23:38:13 -0400

    Updates

---
+++ precache-manifest.94f7bc6bad4418fe64d189627f7b6233.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c695c96c3fee5b31bc1c8b9ebcc7d7f",
+    "url": "/PortableReact/index.html"
+  },
+  {
+    "revision": "8d468007aa79f7095294",
+    "url": "/PortableReact/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "7f9e6f3ad16920ffbc80",
+    "url": "/PortableReact/static/js/2.2ef080de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PortableReact/static/js/2.2ef080de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d468007aa79f7095294",
+    "url": "/PortableReact/static/js/main.dc52ee4d.chunk.js"
+  },
+  {
+    "revision": "14a05d8eb46b306c1055",
+    "url": "/PortableReact/static/js/runtime-main.f4f2e077.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/PortableReact/static/media/logo.ee7cd8ed.svg"
+  }
+]);
